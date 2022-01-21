d0143a2dd2d7d1a84b9c7bfce3606ab6a9c24dc8
Gregory <gregory280@hotmail.com>
2022-01-20 22:25:40 -0300

    modulo 5 finalizado

---
+++ components/withAuthAdmin/index.tsx
@@ -0,0 +1,37 @@
+import { Component } from "react";
+
+import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
+import Cookie from 'js-cookie';
+
+import AuthState from '../../dtos/AuthState';
+import User from '../../dtos/User';
+import ApiData from '../../dtos/ApiData';
+
+const withAuthAdmin = (Component) => {
+  const Auth = (props) => {
+    const router = useRouter();
+    const loggedUser: User = useSelector((state: AuthState) => state.auth.loggedUser);
+    
+    const apiDataCookie = Cookie.get('@api-data');
+    const apiData: ApiData = apiDataCookie ? JSON.parse(apiDataCookie) : null;
+  
+    if(!loggedUser || 
+      loggedUser.profile !== 'admin' ||
+      !apiData ||
+      !apiData['access-token'] ||
+      apiData['aceess-token'] === '') {
+      router.push('/Auth/Login')
+    }
+  
+    return <Component {...props} />;
+  }
+
+  if(Component.getServerSideProps) {
+    Auth.getServerSideProps = Component.getServerSideProps;
+  }
+
+  return Auth;
+}
+
+export default withAuthAdmin;
---
+++ dtos/ApiData.ts
@@ -0,0 +1,7 @@
+export default interface ApiData {
+  'access-token': string,
+  client: string;
+  expiry: string;
+  'token-type': string;
+  uid: string;
+}
